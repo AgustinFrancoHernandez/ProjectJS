d95799226437d366fd30f3323226f2b45daac9c7
Agustín Franco Hernandez <115629791+AgustinFrancoHernandez@users.noreply.github.com>
2022-12-21 13:07:35 -0300

    Simulador step 1

--- main.js
+++ main.js
@@ -1,40 +1,57 @@
-// VARIABLES
+function comprarProducto() {
+    let seleccion = prompt("Bienvenido, necesita un equipo de computo? Indique si o no").toUpperCase();
+    while (seleccion != "SI" && seleccion != "NO") {
+        alert("Dato incorrecto, por favor ingrese si o no");
+        seleccion = prompt("Le interesa comprar?").toUpperCase();
+    };
+    if (seleccion === "SI") {
 
-// let producto = prompt ("Tenemos para ofrecerte una Notebook o una PC de Escritorio, a continuación elije entre opciones 1 y/o 2");
-// let cantidad = parseInt(prompt ("Unidades a comprar?"))
+        alert("Tenemos una Notebook para USD690, o un Pc de escritorio para USD1300");
+        do {
+            producto = prompt("Que equipo te interesa? Notebook o PC");
 
-let productoUno = "notebook";
-let productoDos = "pcescritorio";
-let precioUno = 690;
-let precioDos = 1300;
+            switch (producto) {
+                case "notebook":
+                    price = 690;
+                    break;
+                case "pc" || "pc escritorio":
+                    price = 1300;
+                    break;
+                default:
+                    alert("No has ingresado un dato correcto");
 
-let seleccion = prompt ("Bienvenido, necesita un equipo de computo? Indique si o no");
-while(seleccion != "si" && seleccion != "no"){
-    alert ("Dato incorrecto, por favor ingrese si o no");
-    seleccion = prompt("Le interesa comprar?");
-};
+            }
+        } while (producto != "notebook" && producto != "pc" && producto != "pc escritorio");
 
-alert("Tenemos una Notebook para USD690, o un Pc de escritorio para USD1300");
+        cantidad = parseInt(prompt("Unidades a comprar?"));
+        let cantidadValidada = validarCantidad(cantidad);
+        // agregarMas = confirm("Queres agregar mas Unidades?");  
 
-do {
-    producto = prompt ("Que equipo te interesa? Notebook o PC");
-    cantidad = parseInt(prompt ("Unidades a comprar?"));
+        let hayCuotas = confirm("Queres pagar en cuotas?");
+        if (hayCuotas) {// si quiere comprar en cuotas
+            cuotas = parseInt(prompt("Podes pagar hasta en 6 cuotas, escribe el numero de cuotas a continuacion!"));
+            while (Number.isNaN(cuotas) || cuotas < 1 || cuotas > 6) {
+                alert("Ingrese un valor del 1 al 6");
+                cuotas = parseInt(prompt("Podes pagar hasta en 6 cuotas, escribe el numero de cuotas a continuacion!"));
+            }
+            totalCompra = price * cantidadValidada;
+            totalCompraEnCuotas = totalCompra / cuotas;
+            alert("El total de la compra es USD" + totalCompra + " en "+cuotas+ " cuotas de USD"+totalCompraEnCuotas);
+        } else{
+            totalCompra = price * cantidadValidada;
+            alert("El total de la compra es USD" + totalCompra);
+        }
+        alert("Gracias por su compra!")
+    } else (alert("Goodbye"));
+}
 
-    switch (producto) {
-        case "notebook":
-            price = 690;
-            break;
-        case "pc" || "pc escritorio":
-            price = 1300;
-            break;
-        default:
-            alert("No has ingresado un dato correcto");
-            precio = 0;
-            cantidad = 0;
+function validarCantidad(cantidad) {
+    while (Number.isNaN(cantidad) || cantidad < 1) {
+        alert("Dato incorrecto, debe agregar un numero que sea mayor a 0.");
+        cantidad = parseInt(prompt("Cuantas unidades a comprar?"));
     }
 
-    totalCompra = price * cantidad
-    alert ("El total de la compra es "+totalCompra)
-    agregarMas = confirm ("Queres agregar mas Unidades?");
+    return cantidad;
+};
 
-} while (agregarMas)
+comprarProducto();
