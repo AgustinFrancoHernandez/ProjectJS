5cffad95bf8e59fa4a81453322c924550df8868c
xFranGus <francoagustin990.com>
2023-01-11 12:13:20 -0300

    Segunda entrega

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-alert ("Bienvenid@, a continuación se mostrará una lista de productos y sus precios")
+alert ("Bienvenid@ a TechPC, en la siguiente lista se mostrará un catálogo de equipos Gaming de escritorio.")
 
 const carrito = []
 
 const ordenarProductos = () => {
-    productos.sort((a, b) => a.price - b.price)
+    productos.sort((a, b) => a.precio - b.precio)
     mostrarProductos ()
 };
 
 const mostrarProductos = () => {
     const listaProductos = productos.map(producto => {
-        return "- "+producto.nombre+" USD"+producto.price
+        return "- "+producto.nombre+": "+producto.descripcion+ "\nUSD"+producto.precio
     })
     alert("Lista de productos y precios:"+"\n\n"+listaProductos.join("\n"))
     comprarProductos(listaProductos)
@@ -20,25 +20,41 @@ const comprarProductos = (listaProductos) => {
     let productoCantidad = 0;
     let otroProducto = false;
     
-
     do {
         productoNombre = prompt("Que equipo te interesa comprar?"+"\n\n"+listaProductos.join("\n"))
-        
+
         const producto = productos.find(producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())
         if (producto) {
+            productoCantidad = parseInt(prompt("Cuantas unidades queres comprar?"))
+            productoCantidad = validarCantidad(productoCantidad)
+            
             agregarAlCarrito(producto, producto.id, productoCantidad)
         } else {
             do {
                 alert("El producto no existe!");
                 productoNombre = prompt("Escoja un producto de la lista:"+"\n\n"+listaProductos.join("\n"));
             } while (productos.find(producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())==undefined);
+
+            productoCantidad = parseInt(prompt("Cuantas unidades queres comprar?"))
+            productoCantidad = validarCantidad(productoCantidad)
+
+            const producto = productos.find(producto => producto.nombre.toLowerCase() === productoNombre.toLowerCase())
+            agregarAlCarrito(producto,producto.id,productoCantidad);
         }
              
-        productoCantidad = parseInt(prompt("Cuantas unidades queres comprar?"))
-        validarCantidad(productoCantidad)
-        
-        otroProducto = confirm ("Continuar comprando(ACEPTAR) o pasar a pagar(CANCELAR)")
+        otroProducto = confirm ("Agregar más productos >>[ACEPTAR]"+"\nVer pedido >>[CANCELAR]")
     } while (otroProducto);
+
+    confirmarCompra()
+};
+
+function validarCantidad(productoCantidad) {
+    while (Number.isNaN(productoCantidad) || productoCantidad < 1) {
+        alert("Dato incorrecto, debe agregar un número que sea mayor a 0.");
+        productoCantidad = parseInt(prompt("Cuantas unidades a comprar?"));
+    }
+
+    return productoCantidad;
 };
 
 const agregarAlCarrito = (producto, productoId, productoCantidad) => {
@@ -51,68 +67,49 @@ const agregarAlCarrito = (producto, productoId, productoCantidad) => {
     }
 };
 
-function validarCantidad(productoCantidad) {
-    while (Number.isNaN(productoCantidad) || productoCantidad < 1) {
-        alert("Dato incorrecto, debe agregar un numero que sea mayor a 0.");
-        productoCantidad = parseInt(prompt("Cuantas unidades a comprar?"));
-    }
+const eliminarProductoCarrito = (nombreProductoAEliminar) => {
+    carrito.forEach((producto, index) => {
+        if(producto.nombre.toLowerCase()=== nombreProductoAEliminar.toLowerCase()){
+              if (producto.cantidad > 1) {
+                producto.cantidad-- 
+              } else {
+                carrito.splice(index, 1)
+              }
+        }
+    })
 
-    return productoCantidad;
+    confirmarCompra()
 };
 
+const confirmarCompra = () => {
+    const listaDeProductos = carrito.map(producto => {
+        return "- "+producto.nombre+": "+producto.descripcion+"\n Precio/u USD "+producto.precio+" | Cantidad: "+producto.cantidad
+    })
+
+    const isConfirmar = confirm("Productos agregados: "
+        +"\n\n"+listaDeProductos.join("\n")
+        +"\n\nPara finalizar el pedido presione >>[ACEPTAR]"+"\nPara eliminar uno o más productos de la lista presione >>[CANCELAR]"
+    )
+
+    if (isConfirmar) {
+        finalizarCompra(listaDeProductos)
+    } else {
+        const productoAEliminar = prompt("Ingrese el producto a eliminar"
+        +"\n\n"+listaDeProductos.join("\n"))
+        eliminarProductoCarrito(productoAEliminar)
+    }
+};
+
+const finalizarCompra = (listaDeProductos) => {
+    const cantidadTotal = carrito.reduce((acc, item) => acc + item.cantidad, 0)
+    const precioTotal = carrito.reduce((acc, item) => acc + (item.cantidad * item.precio), 0)
+    alert("Detalle de su pedido: "
+        +"\n\n"+listaDeProductos.join("\n")
+        +"\n\nTotal de productos: "+cantidadTotal
+        +"\n\nEl total a pagar es: "+"USD "+precioTotal
+        +"\n\nGracias por su pedido, l@ contactaremos a la brevedad!"
+    
+    )
+}; 
+
 ordenarProductos ()
-
-
-
-
-
-// function comprarProducto() {
-//     let seleccion = prompt("Bienvenido, necesita un equipo de computo? Indique si o no").toUpperCase();
-//     while (seleccion != "SI" && seleccion != "NO") {
-//         alert("Dato incorrecto, por favor ingrese si o no");
-//         seleccion = prompt("Le interesa comprar?").toUpperCase();
-//     };
-//     if (seleccion === "SI") {
-
-//         alert("Tenemos una Notebook para USD690, o un Pc de escritorio para USD1300");
-//         do {
-//             producto = prompt("Que equipo te interesa? Notebook o PC");
-
-//             switch (producto) {
-//                 case "notebook":
-//                     price = 690;
-//                     break;
-//                 case "pc" || "pc escritorio":
-//                     price = 1300;
-//                     break;
-//                 default:
-//                     alert("No has ingresado un dato correcto");
-
-//             }
-//         } while (producto != "notebook" && producto != "pc" && producto != "pc escritorio");
-
-        // cantidad = parseInt(prompt("Unidades a comprar?"));
-        // let cantidadValidada = validarCantidad(cantidad);
-        // agregarMas = confirm("Queres agregar mas Unidades?");  
-
-//         let hayCuotas = confirm("Queres pagar en cuotas?");
-//         if (hayCuotas) {// si quiere comprar en cuotas
-//             cuotas = parseInt(prompt("Podes pagar hasta en 6 cuotas, escribe el numero de cuotas a continuacion!"));
-//             while (Number.isNaN(cuotas) || cuotas < 1 || cuotas > 6) {
-//                 alert("Ingrese un valor del 1 al 6");
-//                 cuotas = parseInt(prompt("Podes pagar hasta en 6 cuotas, escribe el numero de cuotas a continuacion!"));
-//             }
-//             totalCompra = price * cantidadValidada;
-//             totalCompraEnCuotas = totalCompra / cuotas;
-//             alert("El total de la compra es USD" + totalCompra + " en "+cuotas+ " cuotas de USD"+totalCompraEnCuotas);
-//         } else{
-//             totalCompra = price * cantidadValidada;
-//             alert("El total de la compra es USD" + totalCompra);
-//         }
-//         alert("Gracias por su compra!")
-//     } else (alert("Goodbye"));
-// }
-
-
-
-// comprarProducto();
--- stock.js
+++ stock.js
@@ -2,33 +2,33 @@ const productos =
 [
     {
         id: 1,
-        nombre: "1" || "PC1",
-        descripcion: "Pc pentium",
-        price: 400,
+        nombre: "1",
+        descripcion: "Equipo Intel Pentium G6405 / 8GB ram",
+        precio: 400,
         cantidad: 0
     },
 
     {
         id: 2,
-        nombre: "2" || "PC2",
-        descripcion: "Pc I3",
-        price: 800,
+        nombre: "2",
+        descripcion: "Equipo Intel Core I3 12100F Pro Gamer / 16Gb / SSD / GTX1650",
+        precio: 850,
         cantidad: 0
     },
 
     {
         id: 3,
-        nombre: "3" || "PC3",
-        descripcion: "Pc I5",
-        price: 1400,
+        nombre: "3",
+        descripcion: "Equipo Intel Core I5 12400F Full Gamer / 16Gb / SSD / RTX3050",
+        precio: 1325,
         cantidad: 0
     },
 
     {
         id: 4,
-        nombre: "4" || "PC4",
-        descripcion: "Pc I7",
-        price: 1750,
+        nombre: "4",
+        descripcion: "Equipo Intel Core I7 12700KF Full Gamer / 16Gb / SSD / RTX3070",
+        precio: 2400,
         cantidad: 0
     }
 ]
