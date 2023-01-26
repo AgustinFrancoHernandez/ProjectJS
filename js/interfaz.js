0e7fb1215e6fc881f0ffa1822526bbbf959236bc
Agustin Franco <francoagustin990@gmail.com>
2023-01-26 11:17:53 -0300

    revert

--- js/interfaz.js
+++ js/interfaz.js
@@ -1,31 +1,31 @@
-//Show the product
-const conteinerProducts = document.querySelector("#shop-content")
-function loadProduct () {
-    products.forEach(product =>{
-        let div = document.createElement("div");
-        div.classList.add("product-box");
-        div.innerHTML = 
-                    ` 
-                        <img src="${product.imagen}" alt="Imagen del Producto" class="product-img">
-                        <h2 class="product-title">${product.titulo}</h2>
-                        <span class="price">US$ ${product.precio}</span>
-                        <i class='bx bxs-cart-add add-cart'></i>
-                     `;
+// //Show the product
+// const conteinerProducts = document.querySelector("#shop-content")
+// function loadProduct () {
+//     products.forEach(product =>{
+//         let div = document.createElement("div");
+//         div.classList.add("product-box");
+//         div.innerHTML = 
+//                     ` 
+//                         <img src="${product.imagen}" alt="Imagen del Producto" class="product-img">
+//                         <h2 class="product-title">${product.titulo}</h2>
+//                         <span class="price">US$ ${product.precio}</span>
+//                         <i class='bx bxs-cart-add add-cart'></i>
+//                      `;
 
-        conteinerProducts.append(div);
-    })
-}
+//         conteinerProducts.append(div);
+//     })
+// }
 
-// Carrito 
-let cartIcon = document.querySelector('#cart-icon');
-let cart = document.querySelector('.cart');
-let closeCart = document.querySelector('#close-cart');
-// Abrir Carrito
-cartIcon.onclick = () => {
-    cart.classList.add("active");
-};
-// Cerrar Carrito
-closeCart.onclick = () => {
-    cart.classList.remove("active");
-};
+// // Carrito 
+// let cartIcon = document.querySelector('#cart-icon');
+// let cart = document.querySelector('.cart');
+// let closeCart = document.querySelector('#close-cart');
+// // Abrir Carrito
+// cartIcon.onclick = () => {
+//     cart.classList.add("active");
+// };
+// // Cerrar Carrito
+// closeCart.onclick = () => {
+//     cart.classList.remove("active");
+// };
 
