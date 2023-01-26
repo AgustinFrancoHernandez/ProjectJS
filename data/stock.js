7424f4bd2c10af91c0a7b5ae86c8340ea9455c4d
Agustin Franco <francoagustin990@gmail.com>
2023-01-26 10:54:46 -0300

    organizando-sin localstorage

---
+++ data/stock.js
@@ -0,0 +1,66 @@
+const products = 
+[
+    {
+        id: "pc1-pentium",
+        titulo: "Equipo Intel Pentium G6405 / 8GB Ram",
+        imagen: "./images/pc/pentiumg6400.jpg",
+        precio: 350,
+        cantidad: 1
+    },
+
+    {
+        id: "pc2-i3",
+        titulo: "Intel Core I3 12100f / 16Gb / SSD / GTX1650",
+        imagen: "./images/pc/corei312100gtx1650.jpg",
+        precio: 690,
+        cantidad: 1
+    },
+
+    {
+        id: "pc3-i5gtx1650",
+        titulo: "Intel Core I5 10400f / 16gb / SSD / GTX1650",
+        imagen: "./images/pc/i510400fgtx1650.jpg",
+        precio: 775,
+        cantidad: 1
+    },
+
+    {
+        id: "pc4-i5rtx3050",
+        titulo: "Intel Core I5 12400f / 16Gb / SSD / RTX3050",
+        imagen: "./images/pc/i512400frtx3050.jpg",
+        precio: 975,
+        cantidad: 1
+    },
+
+    {
+        id: "pc5-i5rtx3060",
+        titulo: "Intel Core I5 10400f / 16Gb / SSD / RTX3060",
+        imagen: "./images/pc/i510400frtx3060.jpg",
+        precio: 1200,
+        cantidad: 1
+    },
+    
+    {
+        id: "pc6-i712700rtx3070",
+        titulo: "Intel Core I7 12700kf / 16Gb / SSD / RTX3070",
+        imagen: "./images/pc/i712700rtx3070.jpg",
+        precio: 1590,
+        cantidad: 1
+    },
+
+    {
+        id: "pc7-i910900rtx3060",
+        titulo: "Intel Core I9 10900 / 16Gb / SSD / RTX3060Ti",
+        imagen: "./images/pc/i910900rtx3060ti.jpg",
+        precio: 1900,
+        cantidad: 1
+    },
+
+    {
+        id: "pc8-i9rtx4080",
+        titulo: "Intel Core I9 12900kf / 32Gb / SSD / RTX4080",
+        imagen: "./images/pc/i912900rtx4080.jpg",
+        precio: 4250,
+        cantidad: 1
+    }
+];
